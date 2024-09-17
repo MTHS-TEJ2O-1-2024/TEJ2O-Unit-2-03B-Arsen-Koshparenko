c05ae52699e5771edabcaf87172755f1597fe683
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-09-17 16:34:42 -0400

    I'm doing math

--- main.ts
+++ main.ts
@@ -2,7 +2,23 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2024
- * This program says Hello, Arsen
+ * This program does basik math.
 */
 
-basic.showString('Hello, Arsen!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A rectangle has the dimentions of 5cm by 3 cm')
+
+//multiply
+basic.showString('P=(5cm + 3cm)*2 = ' + ((5 + 3) * 2).toString())
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+
+basic.pause(500)
+
+basic.showString('a = 5cm x 3cm = ' + (5 * 3).toString())
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
